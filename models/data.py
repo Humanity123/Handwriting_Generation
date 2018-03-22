01f67a3fb33d03ec3f02d0cd03dba9f500dcd361
Humanity123 <yashagrwl.iitkgp@gmail.com>
2018-03-22 18:57:49 +0530

    Updating data.py

--- models/data.py
+++ models/data.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 def read_data(file_name):
 	''' reads the data and captures all the metadata 
@@ -9,11 +10,12 @@ def read_data(file_name):
 		max_len = max([len(sequence) for sequence in data])
 	return max_len
 
-def generate_dataset(file_name):
+def generate_dataset(file_name, val_split = 0.1, random_seed = 1):
 	''' reads and genertes the dataset from the file '''
 
 	with open(file_name, 'r') as data_file:
 		data = np.load(data_file)
 
-	return data
+	train_data, valid_data =train_test_split(data, test_size = val_split, random_state = random_seed)
+	return train_data, valid_data
 
