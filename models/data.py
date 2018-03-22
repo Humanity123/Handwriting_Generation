a68d6c57921491a6e3dd0e3739f634923d8ff730
Humanity123 <yashagrwl.iitkgp@gmail.com>
2018-03-22 19:19:35 +0530

    Updating data.py

--- models/data.py
+++ models/data.py
@@ -10,12 +10,17 @@ def read_data(file_name):
 		max_len = max([len(sequence) for sequence in data])
 	return max_len
 
-def generate_dataset(file_name, val_split = 0.1, random_seed = 1):
+def generate_dataset(strokes_file_name, sentence_file_name, val_split = 0.1, random_seed = 1):
 	''' reads and genertes the dataset from the file '''
 
-	with open(file_name, 'r') as data_file:
-		data = np.load(data_file)
+	with open(strokes_file_name, 'r') as strokes_data_file:
+		strokes_data = np.load(strokes_data_file)
 
-	train_data, valid_data =train_test_split(data, test_size = val_split, random_state = random_seed)
+	with open(sentence_file_name, 'r') as sentence_data_file:
+		sentence_data = sentence_data_file.readlines()
+
+ 	train_strokes_data, train_sentence_data, valid_strokes_data, valid_sentence_data = /
+ 		train_test_split(data, sentence_data, test_size = val_split, random_state = random_seed)
+ 		
 	return train_data, valid_data
 
