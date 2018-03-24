5b1a082c95a96f59d810bd96a2df06a6dbbb9e22
Humanity123 <yashagrwl.iitkgp@gmail.com>
2018-03-24 07:24:00 +0530

    Updating data.py

--- models/data.py
+++ models/data.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 from sklearn.model_selection import train_test_split
 
 def read_data(file_name):
@@ -10,17 +11,20 @@ def read_data(file_name):
 		max_len = max([len(sequence) for sequence in data])
 	return max_len
 
-def generate_dataset(strokes_file_name, sentence_file_name, val_split = 0.1, random_seed = 1):
+def generate_dataset_uncond(strokes_file_name, seq_len, val_split = 0.1, random_seed = 1):
 	''' reads and genertes the dataset from the file '''
 
 	with open(strokes_file_name, 'r') as strokes_data_file:
 		strokes_data = np.load(strokes_data_file)
+		chopped_strokes_data = []
 
-	with open(sentence_file_name, 'r') as sentence_data_file:
-		sentence_data = sentence_data_file.readlines()
+		for idx in range(strokes_data.shape[0]):
+			num_chunks = math.ceil( 1. * strokes_data[idx].shape[0] / seq_len)
+			chopped_strokes_data.extend(np.array_split(strokes_data[idx], num_chunks))
 
- 	train_strokes_data, train_sentence_data, valid_strokes_data, valid_sentence_data = /
- 		train_test_split(data, sentence_data, test_size = val_split, random_state = random_seed)
- 		
-	return train_strokes_data, train_sentence_data, valid_strokes_data, valid_sentence_data
+	
+	input_data, target_data = zip(*[ (x[:-1], x[1:]) for x in chopped_strokes_data])
+	train_input_data, valid_input_data, train_target_data, valid_target_data = \
+ 		train_test_split(input_data, target_data, test_size = val_split, random_state = random_seed)
 
+ 	return train_input_data, valid_input_data, train_target_data, valid_target_data
