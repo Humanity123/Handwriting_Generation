005512fc7824ba63c11e4352eeb9b43775f29437
Humanity123 <yashagrwl.iitkgp@gmail.com>
2018-03-24 07:23:36 +0530

    Adding Model Saving Functionality and Bug Fix fir normal bivariate

--- models/model.py
+++ models/model.py
@@ -6,7 +6,7 @@ import math
 class MixtureDensityNetwork(object):
 	''' class for Mixture Density Network '''
 
-	def __init__(self, config, sess):
+	def __init__(self, config, sess, training = True):
 		''' initialisation function for class '''
 
 		self.sess = sess
@@ -20,6 +20,12 @@ class MixtureDensityNetwork(object):
 		self.epochs = config.epochs
 		self.eps = config.eps
 		self.bias = config.bias
+		self.saved_model_directory = config.saved_model_directory
+
+
+		if not training:
+			self.batch_size = 1
+			self.seq_len = 1
 
 		self.Mixture_dim = 1 + self.mixture_components * 6
 
@@ -34,10 +40,10 @@ class MixtureDensityNetwork(object):
 			cons_pi = tf.constant(math.pi)
 			norm_x1 = (x1 - mu1) / sigma1
 			norm_x2 = (x2 - mu2) / sigma2
-			Z = tf.square(norm_x1) + tf.square(norm_x2) - (norm_x1 * norm_x2) 
-			C = 1.0 / (1.0 - tf.square(rho))
+			Z = tf.square(norm_x1) + tf.square(norm_x2) - (2. * rho * (norm_x1 * norm_x2))
+			C = 1.0 / (1.0 - tf.square(rho) )
 
-			normal_bivariate_prob = (1.0 / (2 * cons_pi * sigma1 * sigma2)) * tf.sqrt(C) * tf.exp((-1 * Z) * C)
+			normal_bivariate_prob = (1.0 / (2. * cons_pi * sigma1 * sigma2)) * tf.sqrt(C) * tf.exp((-1 * Z) * C / 2.)
 			return normal_bivariate_prob
 
 		def get_mixture_parameters(y, bias = 0.):
@@ -59,20 +65,25 @@ class MixtureDensityNetwork(object):
 			''' function to calculate the loss '''
 
 			eos_prob = tf.multiply(eos, e) + tf.multiply((1. - eos), (1. - e))
-			eos_prob_2dim = tf.reshape(eos_prob, [self.batch_size, self.seq_len - 1])
+			eos_prob_2dim = tf.reshape(eos_prob, [self.batch_size, self.seq_len])
+			self.check1 = eos_prob_2dim
 
 			pi_prob  = tf.multiply(pi, normal_bivariate_prob)
+			self.check2 = pi_prob
 			reduced_pi_prob = tf.reduce_sum(pi_prob, axis = 2)
+			self.check3 = reduced_pi_prob
 
 			loss_per_timestep = tf.multiply(reduced_pi_prob, eos_prob_2dim)
-			loss_per_timestep_2dim = tf.reshape(loss_per_timestep, [self.batch_size, self.seq_len - 1])
+			loss_per_timestep_2dim = tf.reshape(loss_per_timestep, [self.batch_size, self.seq_len])
 			masked_loss_per_timestep = tf.multiply(mask, loss_per_timestep_2dim)
 			loss = tf.reduce_sum(masked_loss_per_timestep, axis = 1)
+			self.see = loss
 			log_loss = -1 * tf.reduce_sum(tf.log(loss), axis = 0)
 			return log_loss
 
 
 		self.input  = tf.placeholder(tf.float32, [None, self.seq_len, 3], name = 'input')
+		self.target  = tf.placeholder(tf.float32, [None, self.seq_len, 3], name = 'target')
 		self.mask   = tf.placeholder(tf.float32, [None, self.seq_len], name = 'mask')
 
 		ML_LSTM_output_W = tf.get_variable('ML_LSTM_output_W', [self.LSTM_outdim, self.Mixture_dim])
@@ -85,7 +96,7 @@ class MixtureDensityNetwork(object):
 		ML_LSTM_cell = tf.nn.rnn_cell.MultiRNNCell(LSTM_cells, state_is_tuple = True) #batch * layer * LSTM_outdim
 		self.init_state = ML_LSTM_cell.zero_state(self.batch_size, tf.float32)
 
-		l = tf.unstack(zero_state, axis=0)
+		l = tf.unstack(self.init_state, axis=0)
 		rnn_tuple_state = tuple([tf.nn.rnn_cell.LSTMStateTuple(l[idx][0],l[idx][1]) for idx in range(self.LSTM_layers)])
 
 		ML_LSTM_output, layer_LSTM_state = tf.nn.dynamic_rnn(ML_LSTM_cell, self.input, initial_state = rnn_tuple_state) #batch * time * layer * LSTM_outdim
@@ -102,17 +113,17 @@ class MixtureDensityNetwork(object):
 		reduced_out = W_multiplied_out_4dim #batch time mix_dim
 
 		y = tf.add(reduced_out, til_output_b) # batch time mix_dim
-		y_except_last = y[:, :-1, :]
-		self.pi, self.mu1, self.mu2, self.sigma1, self.sigma2, self.rho, self.e = get_mixture_parameters(y_except_last, self.bias)
+		self.pi, self.mu1, self.mu2, self.sigma1, self.sigma2, self.rho, self.e = get_mixture_parameters(y, self.bias)
 
-		x1, x2, eos = tf.split(axis = 2, num_or_size_splits = 3, value = self.input)
+		eos, x1, x2 = tf.split(axis = 2, num_or_size_splits = 3, value = self.target)
 
-		til_x1_t_plus_1  = tf.tile(x1[:, 1:, :], [1, 1, self.mixture_components])
-		til_x2_t_plus_1  = tf.tile(x2[:, 1:, :], [1, 1, self.mixture_components])
-		eos_t_plus_1 = eos[:, 1:, :]
+		til_x1  = tf.tile(x1, [1, 1, self.mixture_components])
+		til_x2  = tf.tile(x2, [1, 1, self.mixture_components])
+		eos     = eos
 
-		normal_bivariate_prob = normal_bivariate(til_x1_t_plus_1, til_x2_t_plus_1, self.mu1, self.mu2, self.sigma1, self.sigma2, self.rho) #batch time matrix_com
-		self.loss = get_loss(normal_bivariate_prob, self.pi, eos_t_plus_1, self.e, self.mask[:, :-1])
+		normal_bivariate_prob = normal_bivariate(til_x1, til_x2, self.mu1, self.mu2, self.sigma1, self.sigma2, self.rho) #batch time matrix_com
+		self.check4 = normal_bivariate_prob
+		self.loss = get_loss(normal_bivariate_prob, self.pi, eos, self.e, self.mask)
 
 		self.lr = tf.Variable(self.init_lr, trainable=False)
 		self.opt = tf.train.AdagradOptimizer(self.lr)
@@ -126,70 +137,100 @@ class MixtureDensityNetwork(object):
 			self.optim = self.opt.apply_gradients(clipped_grads_and_vars)
 
   		self.sess.run(tf.initialize_all_variables())
+  		self.saver = tf.train.Saver()
 
-	def train(self, strokes_train_data):
+	def train(self, strokes_train_data, strokes_target_data):
 		''' function to train the model '''
-		train_input = np.ndarray([self.batch_size, self.seq_len, 3], np.float32)
-		print train_input.shape
-		train_mask  = np.ndarray([self.batch_size, self.seq_len], np.float32)
 
+		train_input  = np.ndarray([self.batch_size, self.seq_len, 3], np.float32)
+		train_target = np.ndarray([self.batch_size, self.seq_len, 3], np.float32)
+		print train_input.shape
+		train_mask   = np.ndarray([self.batch_size, self.seq_len], np.float32)
+
+		best_loss = np.inf
 		for epoch in range(self.epochs):
 			num_batches = int(math.ceil(len(strokes_train_data) / self.batch_size))
 			rand_idx, cur = np.random.permutation(len(strokes_train_data)), 0
 
+			total_loss_per_epoch = 0.
 			for batch_idx in range(num_batches):
 				train_input.fill(0.)
 				train_mask.fill(0.)
 
 				for idx in range(self.batch_size):
 					stroke_idx = rand_idx[cur]
-					stroke_data = strokes_train_data[stroke_idx]
-					train_input[idx, :stroke_data.shape[0], :] = stroke_data
-					train_mask[idx, :stroke_data.shape[0]].fill(1.)
+
+					stroke_input_data = strokes_train_data[stroke_idx]
+					train_input[idx, :stroke_input_data.shape[0], :] = stroke_input_data
+
+
+					stroke_target_data = strokes_target_data[stroke_idx]
+					train_target[idx, :stroke_target_data.shape[0], :] = stroke_target_data
+
+					train_mask[idx, :stroke_input_data.shape[0]].fill(1.)
 					cur += 1
 
-				loss, _ = self.sess.run( [self.loss, self.optim], feed_dict = {
+				c1,c2,c3, c4, s, loss, _ = self.sess.run( [self.check1, self.check2, self.check3, self.check4, self.see, self.loss, self.optim], feed_dict = {
 																	self.input : train_input,
+																	self.target: train_target,
 																	self.mask  : train_mask})
+				total_loss_per_epoch += loss
+				print loss
+				print np.any(c4 > 1)
+				print np.any(c1 < 0)
+				print np.any(c2 < 0)
+				print np.any(c3 < 0)
 
-				print('epoch=%d train-loss=%.2f;' % (epoch, loss))
+				if batch_idx == 5:
+					self.saver.save(self.sess, self.saved_model_directory, global_step = epoch)
+			print('epoch=%d train-loss=%.2f;' % (epoch, loss))
+			if total_loss_per_epoch < best_loss:
+				best_loss = total_loss_per_epoch
+				self.saver.save(self.sess, self.saved_model_directory, global_step = epoch)
 
 
 	def synthesize(self, length):
 		''' function to generate unconditional text '''
+
 		def generate_sample(parameters):
-			''' generates a sample of the mixture model of bivariate gaussian and bernoulli'''
+			''' generates a sample of the mixture model of bivariate gaussian and bernoulli '''
 			
 			pi, mu1, mu2, sigma1, sigma2, rho, e = parameters
 			random_number = np.random.random()
 
 			cumsum_pi = np.cumsum(pi)
-			mixture_idx = next(x[0] for x in enumerate(cumsum_pi) if x[1] >= random_number)
+			try:
+				mixture_idx = next(x[0] for x in enumerate(cumsum_pi) if x[1] >= random_number)
+			except:
+				mixture_idx = self.mixture_components - 1 
+				print mixture_idx
 
 			eos = 1 if e >= random_number else 0
-			mean = [mu1, mu2]
-            cov = [[sigma1 * sigma1, rho * sigma1 * sigma2], [rho * sigma1 * sigma2, sigma2 * sigma2]]
-            x1, x2 = np.random.multivariate_normal(mean, cov)
 
-            return x1, x2, eos
+			mix_pi, mix_mu1, mix_mu2, mix_sigma1, mix_sigma2, mix_rho = [param[0][0][mixture_idx] for param in parameters[:-1]]
+			mean = [mix_mu1, mix_mu2]
+			cov = [[mix_sigma1 * mix_sigma1, mix_rho * mix_sigma1 * mix_sigma2], [mix_rho * mix_sigma1 * mix_sigma2, mix_sigma2 * mix_sigma2]]
+			x1, x2 = np.random.multivariate_normal(mean, cov)
+
+			return eos, x1, x2
 
 		stroke_data = np.zeros([1, length, 3])
 		mask_data = np.ones([1, length])
 
-		strokes_data[0][2] = 1
+		stroke_data[0][0] = 1
 
-		init_state = self.init_state.eval()
+		init_state = self.sess.run(self.init_state)
 
 		for stroke_idx in range(length-1):
 			init_state, pi, mu1, mu2, sigma1, sigma2, rho, e = self.sess.run([self.init_state, self.pi, self.mu1, self.mu2, self.sigma1, 
-																	self.sigma2, self.rho, self.e]. feed_dict = {
-																	self.input : strokes_data[:, stroke_idx, :],
-																	self.mask : mask_data[:, idx],
+																	self.sigma2, self.rho, self.e], feed_dict = {
+																	self.input : stroke_data[:, stroke_idx: stroke_idx+1, :],
+																	self.mask : mask_data[:, stroke_idx: stroke_idx+1],
 																	self.init_state : init_state})
-			strokes_data[stroke_idx, :] = generate_sample((pi, mu1, mu2, sigma1, sigma2, rho, e))
+			stroke_data[0, stroke_idx, :] = generate_sample((pi, mu1, mu2, sigma1, sigma2, rho, e))
 
 
-		return strokes_data
+		return stroke_data[0]
 
 
 
