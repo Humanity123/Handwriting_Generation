d40557807fd54dfe9e14bd1b981f358eaa71cdbe
Humanity123 <yashagrwl.iitkgp@gmail.com>
2018-03-25 19:06:54 +0530

    Adding Generate Dataset Function for Coniditional Model

--- models/data.py
+++ models/data.py
@@ -12,7 +12,9 @@ def read_data(file_name):
 	return max_len
 
 def generate_dataset_uncond(strokes_file_name, seq_len, val_split = 0.1, random_seed = 1):
-	''' reads and genertes the dataset from the file '''
+	''' reads and genertes the dataset from the file for unconditional model
+		returns the tuple - 
+		(train_input_data, valid_input_data, train_target_data, valid_target_data) '''
 
 	with open(strokes_file_name, 'r') as strokes_data_file:
 		strokes_data = np.load(strokes_data_file)
@@ -24,7 +26,26 @@ def generate_dataset_uncond(strokes_file_name, seq_len, val_split = 0.1, random_
 
 	
 	input_data, target_data = zip(*[ (x[:-1], x[1:]) for x in chopped_strokes_data])
-	train_input_data, valid_input_data, train_target_data, valid_target_data = \
+	dataset = \
  		train_test_split(input_data, target_data, test_size = val_split, random_state = random_seed)
 
- 	return train_input_data, valid_input_data, train_target_data, valid_target_data
+ 	return dataset
+
+def generate_dataset_cond(strokes_file_name, sentences_file_name, random_seed = 1):
+	''' reads and genertes the dataset from the file for conditional model
+		return the tuple - 
+		(train_input_data, valid_input_data, train_target_data, valid_target_data, 
+		train_sentence_data, valid_sentence_data)'''
+
+	with open(strokes_file_name, 'r') as strokes_data_file:
+		strokes_data = np.load(strokes_data_file)
+	
+	with open(sentences_file_name, 'r') as sentences_data_file:
+		sentences_data = np.load(sentences_data_file)
+
+	input_data, target_data = zip(*[ (x[:-1], x[1:]) for x in chopped_strokes_data])
+	dataset = \
+ 		train_test_split(input_data, target_data, sentences_data, test_size = val_split, random_state = random_seed)
+
+ 	return dataset
+
